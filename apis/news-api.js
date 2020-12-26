d25e573e804c36d9830bbeba4ae32e08d884ee1e
abhikam395 <abhikam395@gmail.com>
2020-12-26 12:43:41 +0530

    dummy data added

--- apis/news-api.js
+++ apis/news-api.js
@@ -7,7 +7,6 @@ let array = new Array(20).fill(getDummyData())
 
 export function getNews(category){
     let api = getApi(category);
-<<<<<<< HEAD
     // fetch(api).then(res => {
     //     // setTimeout(() => {
     //     //     if(res.articles != undefined)
@@ -29,26 +28,4 @@ export function getNews(category){
     //     console.log(err)
     // })
     store.dispatch(newsAction(array))
-=======
-    fetch(api).then(res => {
-        // setTimeout(() => {
-        //     if(res.articles != undefined)
-        //         store.dispatch(newsAction(array))
-        // }, 5000)
-        // console.log(api)
-        // if (res.status !== 200) {
-        //     console.log('Looks like there was a problem. Status Code: ' +
-        //     res.status);
-            
-        //     return;
-        // }   
-        // res.json().then(data => {
-        //     store.dispatch(newsAction(data.articles))
-        // })
-        store.dispatch(newsAction(array))
-    })
-    .catch(err => {
-        console.log(err)
-    })
->>>>>>> be89616125d9a4a250ba7de8991590aa4ffc6aea
 }
